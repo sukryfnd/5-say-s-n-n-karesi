86128b60980def9c064b66286dede18bdb0397fe
sukryfnd <fsukru96@gmail.com>
2024-10-20 11:59:15 +0300

    Add files via upload

---
+++ iki sayının farkı.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Birinci sayıyı girin: ");
+        int sayi1 = int.Parse(Console.ReadLine());
+
+        Console.Write("İkinci sayıyı girin: ");
+        int sayi2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Fark: " + (sayi1 - sayi2));
+    }
+}
