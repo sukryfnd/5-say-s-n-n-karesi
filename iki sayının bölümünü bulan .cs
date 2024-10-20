429fc00b67acbe931cd819d26178a58ae09aae8b
sukryfnd <fsukru96@gmail.com>
2024-10-20 12:15:29 +0300

    Add files via upload

---
+++ iki sayının bölümünü bulan .cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Birinci sayıyı girin: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("İkinci sayıyı girin: ");
+        int b = int.Parse(Console.ReadLine());
+
+        // Bölüm ve sıfıra bölme kontrolü
+        Console.WriteLine(b != 0 ? "Bölüm: " + (a / b) : "Sıfıra bölünemez!");
+    }
