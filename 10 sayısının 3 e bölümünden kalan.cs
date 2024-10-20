b2653bf5c8ff455ad007efa41b84258fde51c90e
sukryfnd <fsukru96@gmail.com>
2024-10-20 11:58:44 +0300

    Add files via upload

---
+++ 10 sayısının 3 e bölümünden kalan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sayi = 10;
+        int bolen = 3;
+
+        int kalan = sayi % bolen;
+
+        Console.WriteLine($"10 sayısının 3'e bölümünden kalan: {kalan}");
+        Console.ReadKey();
+    }
+}
