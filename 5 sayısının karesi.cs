655ce91b21059b9170566e1ebcb83e40f5a78962
sukryfnd <fsukru96@gmail.com>
2024-10-20 11:35:52 +0300

    Add files via upload

---
+++ 5 sayısının karesi.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sayi = 5;
+        int kare = sayi * sayi; // veya Math.Pow(sayi, 2)
+        Console.WriteLine("5 sayısının karesi: " + kare);
+        Console.ReadKey();
+    }
+}
