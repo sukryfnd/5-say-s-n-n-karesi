4c51aca6bff15d757abb4310d9589ecc198abcf6
sukryfnd <fsukru96@gmail.com>
2024-10-20 12:07:57 +0300

    Add files via upload

---
+++ Klavyeden kenarları girilen dikdörtgenler prizmasının hacmini hesaplayan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Dikdörtgenler prizmasının uzun kenarını girin: ");
+        double uzunKenar = double.Parse(Console.ReadLine());
+
+        Console.Write("Dikdörtgenler prizmasının kısa kenarını girin: ");
+        double kisaKenar = double.Parse(Console.ReadLine());
+
+        Console.Write("Dikdörtgenler prizmasının yüksekliğini girin: ");
+        double yukseklik = double.Parse(Console.ReadLine());
+
+        // Hacmi hesapla
+        double hacim = uzunKenar * kisaKenar * yukseklik;
+
+        Console.WriteLine($"Dikdörtgenler prizmasının hacmi: {hacim}");
+        
+    }
+}
